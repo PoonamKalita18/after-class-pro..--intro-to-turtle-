616d1769c24b4bf769f471094f89a0dc45098d0c
PoonamKalita18 <arunakalita2015@gmail.com>
2024-11-04 20:42:05 +0530

    turtle

---
+++ main.py
@@ -0,0 +1,29 @@
+#to draw a star with the help of turtle.
+import turtle
+
+turtle.Screen().bgcolor("Cyan")
+board= turtle.Turtle()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
